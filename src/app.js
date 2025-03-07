e11918791aa122acde4ad7f874ba554b3564b38d
saikumar14-08 <saikumar9808@gmail.com>
2025-03-07 10:42:47 -0600

    API workings

--- src/app.js
+++ src/app.js
@@ -1,18 +1,43 @@
 const express = require("express");
 const user = require("./models/user");
 const connectDB = require("./config/database");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
-const PORT = 3002;
+const PORT = 3001;
 
-app.post("/signup", (req, res) => {
-  const data = new user({
-    firstName: "pooja",
-    lastName: "Boreddy",
-    age: 26,
-  });
-  data.save();
-  res.send("User added successfully");
+app.use(express.json());
+
+app.post("/signup", async (req, res) => {
+  const reqBody = new user(req.body);
+  console.log(reqBody);
+
+  try {
+    await reqBody.save();
+    res.send("User added successfully");
+  } catch (e) {
+    console.error("User cannot be added");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const lastName = await req.body.lastName;
+  const resUser = await user.find({ lastName: lastName });
+  try {
+    resUser ? res.send(resUser) : res.send("No user matched");
+  } catch (e) {
+    console.log("/user error");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  // const userFeed = req.body;
+  const allUsers = await user.find({});
+  try {
+    !allUsers ? res.status(500).send("No Data in DB") : res.send(allUsers);
+  } catch (err) {
+    console.error("/feed Error");
+  }
 });
 
 connectDB()
